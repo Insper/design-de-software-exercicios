6c5dec07aca6901f595a8c738cb2219ac36f609a
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2020-12-29 22:17:21 -0300

    One more trace

---
+++ traces/lista-de-contatos/code.py
@@ -0,0 +1,4 @@
+telefones = {"Fulano": "99999-1111", "Sicrano": "99999-2222", "Beltrano": "99999-3333"}
+nome = input("Digite um nome: ")
+telefone = telefones[nome]
+print("Telefone de {0} é: {1}".format(nome, telefone))
